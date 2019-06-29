4fd83b8b318da96d54095f6133f09699da60c632
Mihnea Andrei <nate.north97@gmail.com>
2019-06-29 12:52:17 +0100

    Adding cordova dialogs plugin

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -7,10 +7,27 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "SecureStorage"
       ]
+    },
+    {
+      "id": "cordova-plugin-dialogs.notification",
+      "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
+    },
+    {
+      "id": "cordova-plugin-dialogs.notification_android",
+      "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-secure-storage": "3.0.2"
+    "cordova-plugin-secure-storage": "3.0.2",
+    "cordova-plugin-dialogs": "2.0.1"
   };
 });
