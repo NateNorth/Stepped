dd71adfdf264b3bb1e305f18b53a4f2c8d8372db
Mihnea Andrei <nate.north97@gmail.com>
2019-06-29 12:46:32 +0100

    Adding secure storage core plugin

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-secure-storage.SecureStorage",
+      "file": "plugins/cordova-plugin-secure-storage/www/securestorage.js",
+      "pluginId": "cordova-plugin-secure-storage",
+      "clobbers": [
+        "SecureStorage"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-secure-storage": "3.0.2"
   };
 });
