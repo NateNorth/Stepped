0d1afebe6a3e45ee4a95d3819021e11a08838aff
Mihnea Andrei <nate.north97@gmail.com>
2019-06-29 12:56:45 +0100

    Adding pedometer plugin

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -23,11 +23,20 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "merges": [
         "navigator.notification"
       ]
+    },
+    {
+      "id": "cordova-plugin-pedometer.Pedometer",
+      "file": "plugins/cordova-plugin-pedometer/www/pedometer.js",
+      "pluginId": "cordova-plugin-pedometer",
+      "clobbers": [
+        "pedometer"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-secure-storage": "3.0.2",
-    "cordova-plugin-dialogs": "2.0.1"
+    "cordova-plugin-dialogs": "2.0.1",
+    "cordova-plugin-pedometer": "0.4.1"
   };
 });
